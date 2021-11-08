d9a02860d76bd95933c60ec02d77fe0fb145dde2
rejiejay <454766952@qq.com>
2021-11-09 00:17:34 +0800

    add getDetailUrl outputDetailExcel

--- index.mjs
+++ index.mjs
@@ -2,16 +2,96 @@ import request from "superagent";
 import FilesHelper from "./utils/files-helper.mjs";
 import cheerio from "cheerio";
 
-request
-    .get('https://zwfw.spb.gov.cn/mdwd/mdwdDetail?endNodeBeianUserName=&endNodeName=%E5%B9%BF%E4%B8%9C%E7%9C%81%E6%B2%B3%E6%BA%90%E5%B8%82%E8%BF%9E%E5%B9%B3%E5%8E%BF%E5%86%85%E8%8E%9E%E9%95%87%E5%9C%86%E9%80%9A%E5%BF%AB%E9%80%92%E6%9C%8D%E5%8A%A1%E7%82%B9&beianNum=%E7%B2%A4%E9%82%AE%E5%A4%8716201800026')
-    .set('Cookie', 'zwfwCookie=4B1AD6EAE34AE7EA52541499BC976274')
-    .then(async res => {
-        const text = res.text;
-        const fileName = `./output/${new Date().getTime()}.text`
-        await FilesHelper.accumulateText(fileName, text)
+const baseUrl = 'https://zwfw.spb.gov.cn'
+const listPage = (currentPage) => `/mdwd/data?currentPage=${currentPage}&beianUserName=&currentPage=${currentPage}&beianNum=&provinceId=19&cityId=241&currentPage=${currentPage}&endNodeName=&verfcode=i4zu`
 
-        const $ = cheerio.load(text);
-    })
-    .catch(err => {
-        // err.message, err.response
-    });
+const getDetailUrl = currentPage => new Promise(async (resolve, reject) => {
+    request
+        .get(`${baseUrl}${listPage(currentPage)}`)
+        .set('Cookie', 'zwfwCookie=4B1AD6EAE34AE7EA52541499BC976274')
+        .then(async res => {
+            const text = res.text;
+            const $ = cheerio.load(text);
+            const table = $('.right_zt').find('table');
+            const tr = table.find('tr');
+
+            const detailedInformationUrl = tr.filter(function (i, el) {
+                return i !== 0;
+            })
+                .map(function (i, el) {
+                    const element = $(this).find('a');
+
+                    const result = element.map(function (i, el) {
+                        const text = $(this).text()
+                        const href = $(this).attr('href')
+
+                        return {
+                            href,
+                            text
+                        }
+                    }).toArray();
+
+                    return result[0].href;
+                }).toArray();
+
+            setTimeout(() => resolve(detailedInformationUrl), 2000)
+        })
+        .catch(err => {
+            reject(err);
+        });
+})
+
+const outputDetailExcel = url => new Promise(async (resolve, reject) => {
+    request
+        .get(`${baseUrl}${url}`)
+        .set('Cookie', 'zwfwCookie=4B1AD6EAE34AE7EA52541499BC976274')
+        .then(async res => {
+            const text = res.text;
+            const $ = cheerio.load(text);
+            const companyList = $('.resultList').find('.companyList2');
+
+
+            const resultList = companyList.map(function (i, el) {
+                const element = $(this).text();
+
+                return element.trim();
+            }).toArray();
+
+
+            const renderHtml = `${resultList[0]}	${resultList[2]}	${resultList[3]}	${resultList[6]}	${resultList[7]}`;
+
+            const fileName = `./output/输出.text`
+            await FilesHelper.accumulateText(fileName, renderHtml)
+
+            setTimeout(() => resolve(), 2000)
+        })
+        .catch(err => {
+            reject(err);
+        });
+})
+
+const main = async () => {
+    for (let currentPage = 1; currentPage <= 64; currentPage++) {
+        let detailedInformationUrl = [];
+
+        try {
+            detailedInformationUrl = await getDetailUrl(currentPage)
+        } catch (error) {
+            console.error(error);
+            return;
+        }
+
+        for (let index = 0; index <= url; index++) {
+            const url = detailedInformationUrl[index];
+
+            try {
+                await outputDetailExcel(url)
+            } catch (error) {
+                console.error(error);
+                return;
+            }
+        }
+    }
+}
+
+main();
